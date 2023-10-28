c073e4edae92590cd0803ccceef0b5bb94b47beb
Nata Kuznetsova <natanavky@icloud.com>
2023-10-28 21:28:15 +0300

    Firsts Screen ViewController

--- APOD/APOD/SceneDelegate.swift
+++ APOD/APOD/SceneDelegate.swift
@@ -13,12 +13,32 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+       
         guard let _ = (scene as? UIWindowScene) else { return }
+        let tabBarController = UITabBarController()
+        
+        let viewController = FirstScreenViewController()
+    //    let newViewController = NewViewController()
+        
+        viewController.tabBarItem.title = "APOD"
+    //    newViewController.tabBarItem.title = "DateAPOD"
+        
+        
+        let controllers = [viewController, newViewController]
+        
+        tabBarController.viewControllers = controllers
+        
+        let navigationController = UINavigationController(rootViewController: tabBarController)
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
+    
+    
+    
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
